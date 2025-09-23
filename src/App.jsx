2331dd805dec58b2dbcecea5623665fedf4751b4
sesco001 <marcasmatheka@gmail.com>
2025-09-23 01:57:43 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import CategoryPage from "./pages/CategoryPage";
+import Submit from "./pages/Submit";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Admin from "./pages/Admin";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="container mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/category/:id" element={<CategoryPage />} />
+          <Route path="/submit" element={<Submit />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
