a66350b18403f962c0b0fa573c003b31ae0d95f8
sesco001 <marcasmatheka@gmail.com>
2025-10-10 13:41:50 +0300

    Create AdminAssignments.jsx

---
+++ src/src/admin/AdminAssignments.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminAssignments() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-4">📚 Assignments</h1>
+      <p>All uploaded assignments will be displayed here for management.</p>
+    </div>
+  );
+}
